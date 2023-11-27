a1a3c6397d3171d7b77ca600483966078a3d39a0
Kribbz0r <kribbz0r@gmail.com>
2023-11-27 16:39:40 +0100

    Closes #5. Kollar om To Do är sen

--- The Task/src/main/java/com/thetask/demo/ThetaskApplication.java
+++ The Task/src/main/java/com/thetask/demo/ThetaskApplication.java
@@ -1,5 +1,10 @@
 package com.thetask.demo;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,21 @@ public class ThetaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThetaskApplication.class, args);
+		getCurrentTime();
+
+	}
+
+	public static long getCurrentTime() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		ZoneId zoneId = ZoneId.systemDefault();
+		ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zoneId);
+		long currentTime = zonedDateTime.toInstant().toEpochMilli();
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM HH:mm");
+		String formattedDateTime = localDateTime.format(dateFormatter);
+		System.out.println("Time, milliseconds: " + currentTime);
+		System.out.println("Time, formatted: " + formattedDateTime);
+		return currentTime;
+
 	}
 
 }
--- The Task/src/test/java/com/thetask/demo/ThetaskApplicationTests.java
+++ The Task/src/test/java/com/thetask/demo/ThetaskApplicationTests.java
@@ -1,5 +1,8 @@
 package com.thetask.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,14 @@ class ThetaskApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testIfToDoIsLate() {
+		long testTime = 1601097035961L;
+		long testTime2 = 1801097035961L;
+		assertTrue(testTime < ThetaskApplication.getCurrentTime());
+		assertFalse(testTime2 < ThetaskApplication.getCurrentTime());
 	}
 
 }
