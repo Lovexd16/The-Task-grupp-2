2f11dbb0f16ca9ccef3ad7f4b210a60abe99eee9
OncleMonocle <bajram.a99@hotmail.com>
2023-11-30 09:33:48 +0100

    uncommented in js

--- The Task/src/main/resources/static/scriptForLoginPage.js
+++ The Task/src/main/resources/static/scriptForLoginPage.js
@@ -1,18 +1,18 @@
-// let createUserBtn = document.getElementById("createUserBtn");
+let createUserBtn = document.getElementById("createUserBtn");
 
-// let usernameField = document.getElementById("User2");
+let usernameField = document.getElementById("User2");
 
-// createUserBtn.addEventListener("click", function () {
-//     localStorage.setItem("loggedInAs", usernameField.value)
-// });
+createUserBtn.addEventListener("click", function () {
+    localStorage.setItem("loggedInAs", usernameField.value)
+});
 
-// let loginBtn = document.getElementById("loginBtn");
+let loginBtn = document.getElementById("loginBtn");
 
-// let usernameField2 = document.getElementById("User1");
+let usernameField2 = document.getElementById("User1");
 
-// loginBtn.addEventListener("click", function () {
-//     localStorage.setItem("loggedInAs", usernameField2.value)
-// });
+loginBtn.addEventListener("click", function () {
+    localStorage.setItem("loggedInAs", usernameField2.value)
+});
 
 //MÅSTE SKRIVAS OM OCH FIXAS SÅ ATT DEN SPARAS I LOCAL STORAGE BARA EFTER ATT ANVÄNDAREN LYCKATS LOGGA IN ELLER LYCKATS SKAPA KONTO 
 //KANSKE VIKTIGT, VI KAN TA UPP DET TILLSAMMANS
