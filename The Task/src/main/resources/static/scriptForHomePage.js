d27fef0401117b43e42625f2b45ca8eaf94fbfb1
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2023-11-30 10:00:59 +0100

    början på homePage

--- The Task/src/main/resources/static/scriptForHomePage.js
+++ The Task/src/main/resources/static/scriptForHomePage.js
@@ -1,3 +1,14 @@
 let title = document.getElementById("title");
 
 title.textContent = localStorage.getItem("loggedInAs");
+
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var timeInput = document.getElementById('timeInput');
+    var todoArea = document.getElementById('toDoArea');
+    var doneArea = document.getElementById('doneArea')
+    var taskText = taskInput.value;
+    var timeText = + ' - ' + timeText + '\n';
+    taskInput.value = '';
+    timeInput.value = '';
+}
