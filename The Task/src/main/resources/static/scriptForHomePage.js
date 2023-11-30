1c056f35a349e434855e9d5c679f2b9a93331cb9
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2023-11-30 11:27:02 +0100

    html

--- The Task/src/main/resources/static/scriptForHomePage.js
+++ The Task/src/main/resources/static/scriptForHomePage.js
@@ -14,3 +14,17 @@ function addTask() {
     taskInput.value = '';
     timeInput.value = '';
 }
+function newListBtn() {
+    var scrollBox = document.querySelector('.scrollBox');
+    var newListName = prompt('Name of your new list:');
+ 
+    if (newListName) {
+        var newList = document.createElement('p');
+        newList.textContent = '-' + newListName + '-';
+        scrollBox.appendChild(newList);
+    }
+}
+function logout() {
+    localStorage.removeItem('loggedInAs');
+    window.location.href = "/login";
+}
