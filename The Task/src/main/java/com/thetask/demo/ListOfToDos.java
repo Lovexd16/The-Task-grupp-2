1af71310df3602e408cd7dfaf11f4de9650d25ce
Kribbz0r <kribbz0r@gmail.com>
2023-12-04 12:19:46 +0100

    Pill och fix. Bajram

--- The Task/src/main/java/com/thetask/demo/ListOfToDos.java
+++ The Task/src/main/java/com/thetask/demo/ListOfToDos.java
@@ -7,9 +7,60 @@ public class ListOfToDos {
     private ArrayList<ToDo> ListOfToDos;
     private String nameOfList;
 
-    public ListOfToDos (ArrayList<ToDo> ListOfToDos, String nameOfList) {
+    public ListOfToDos(ArrayList<ToDo> ListOfToDos, String nameOfList) {
         this.ListOfToDos = new ArrayList<>();
         this.nameOfList = nameOfList;
+    }
+
+    // FIXA DETTA!!! KNAS!!! *********************************
+    public void removeToDo(String username, String nameOfList, String nameOfToDo, long timeOfToDo) {
+        for (User user : ThetaskApplication.userlist.getUserList()) {
+            if (user.getUsername().equals(username)) {
+                for (ListOfToDos list : user.getToDoLists()) {
+
+                    if (list.getNameOfList().equals(nameOfList)) {
+                        user.getToDoLists().get(user.getToDoLists().indexOf(list)).getListOfToDos().remove()/*
+                                                                                                             * .remove()
+                                                                                                             * loopa
+                                                                                                             * igenom
+                                                                                                             * listan
+                                                                                                             * och
+                                                                                                             * ta bort
+                                                                                                             * om
+                                                                                                             * nameOfToDo
+                                                                                                             * ==
+                                                                                                             * nameOfToDo
+                                                                                                             */;
+                    }
+                }
+            }
+        }
+    }
+
+    // FIXA DETTA!!! KNAS!!! *********************************
+    public void editToDoTime(String username, String nameOfList, String nameOfToDo, long timeOfToDo) {
+        for (User user : ThetaskApplication.userlist.getUserList()) {
+            if (user.getUsername().equals(username)) {
+                for (ListOfToDos list : user.getToDoLists()) {
+                    if (list.getNameOfList().equals(nameOfList)) {
+                        user.getToDoLists().get(user.getToDoLists().indexOf(list)).getListOfToDos();
+                    }
+                }
+            }
+        }
+    }
+
+    // FIXA DETTA!!! KNAS!!! *********************************
+    public void editToDoName(String username, String nameOfList, String nameOfToDo, long timeOfToDo) {
+        for (User user : ThetaskApplication.userlist.getUserList()) {
+            if (user.getUsername().equals(username)) {
+                for (ListOfToDos list : user.getToDoLists()) {
+                    if (list.getNameOfList().equals(nameOfList)) {
+                        user.getToDoLists().get(user.getToDoLists().indexOf(list)).getListOfToDos();
+                    }
+                }
+            }
+        }
     }
 
     public ArrayList<ToDo> getListOfToDos() {
--- The Task/src/main/java/com/thetask/demo/ThetaskApplication.java
+++ The Task/src/main/java/com/thetask/demo/ThetaskApplication.java
@@ -11,33 +11,35 @@ public class ThetaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThetaskApplication.class, args);
 
-		
-
 		userlist.getUserList().add(new User("Kalle", "123"));
 		userlist.getUserList().add(new User("Balle", "222"));
 
-
 		System.out.println(userlist.getUserList().get(0).getUsername());
-		
+
 		addNewListForUser("Kalle", "Hushållssyssllur");
 		addNewListForUser("Kalle", "Ärenden");
 
-		addNewListForUser("Balle", "Shoppa");	
+		addNewListForUser("Balle", "Shoppa");
 		addNewListForUser("Balle", "Träna");
-
 
 		for (User user : userlist.getUserList()) {
 			if (user.getUsername().equals("Kalle")) {
-				System.out.println("namnen på Kalles listor " + user.getToDoLists().get(0).getNameOfList() + " " + user.getToDoLists().get(1).getNameOfList());
+				System.out.println("namnen på Kalles listor " + user.getToDoLists().get(0).getNameOfList() + " "
+						+ user.getToDoLists().get(1).getNameOfList());
 			}
 		}
-		
+
 		addNewToDoForUser("Kalle", "Ärenden", "Inget", 2);
 		addNewToDoForUser("Balle", "Träna", "Axlar", 15);
-		
+		System.out.println(userlist.getUserList().get(1).getToDoLists().get(1).getListOfToDos().get(0).getName());
+
+		ListOfToDos.removeToDo("Balle", "Träna", "Axlar", 1000);
+		// System.out.println("är detta axlar? "
+		// +
+		// userlist.getUserList().get(1).getToDoLists().get(1).getListOfToDos().get(0).getName());
 
 		System.out.println(userlist.getUserList().get(0).getToDoLists().get(1).getListOfToDos().get(0).getName());
-		
+
 	}
 
 	public static void addNewListForUser(String username, String nameOfList) {
@@ -46,14 +48,15 @@ public class ThetaskApplication {
 				user.getToDoLists().add((new ListOfToDos(null, nameOfList)));
 			}
 		}
-	}	
+	}
 
-	public static void addNewToDoForUser (String username, String nameOfList, String nameOfToDo, long timeOfToDo) {
+	public static void addNewToDoForUser(String username, String nameOfList, String nameOfToDo, long timeOfToDo) {
 		for (User user : userlist.getUserList()) {
 			if (user.getUsername().equals(username)) {
 				for (ListOfToDos list : user.getToDoLists()) {
 					if (list.getNameOfList().equals(nameOfList)) {
-						user.getToDoLists().get(user.getToDoLists().indexOf(list)).getListOfToDos().add(new ToDo(nameOfToDo, timeOfToDo, false));
+						user.getToDoLists().get(user.getToDoLists().indexOf(list)).getListOfToDos()
+								.add(new ToDo(nameOfToDo, timeOfToDo, false));
 					}
 				}
 			}
